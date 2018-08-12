0c3bf8bc2a2fa4a642b56342893c70270c07a307
SkyNet <sweetkevindan@gmail.com>
2018-08-12 16:40:04 +0000

    Adding sphere_games

---
+++ sphere_games/plugins/build/CMakeFiles/sphere_drive.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sphere_drive.dir/sphere_drive.cc.o"
+  "libsphere_drive.pdb"
+  "libsphere_drive.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sphere_drive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
